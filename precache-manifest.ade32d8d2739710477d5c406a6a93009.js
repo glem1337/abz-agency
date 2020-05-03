0e805774ad2331185d7003ecb6c93ee532a10e4f
Evhenii Grigoriev <grigorieffevgeniy@gmail.com>
2020-05-03 17:22:27 +0300

    Updates

---
+++ precache-manifest.ade32d8d2739710477d5c406a6a93009.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519b0ea6cb627737bc1febb6a97eb413",
+    "url": "/abz-agency/index.html"
+  },
+  {
+    "revision": "920ed66c79751dd74d5c",
+    "url": "/abz-agency/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "5fca65185af968c42413",
+    "url": "/abz-agency/static/css/main.f0d8afaa.chunk.css"
+  },
+  {
+    "revision": "920ed66c79751dd74d5c",
+    "url": "/abz-agency/static/js/2.876c9938.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/abz-agency/static/js/2.876c9938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fca65185af968c42413",
+    "url": "/abz-agency/static/js/main.f5bd8b75.chunk.js"
+  },
+  {
+    "revision": "97ac0c5e8c7376e5271b",
+    "url": "/abz-agency/static/js/runtime-main.a33d765c.js"
+  },
+  {
+    "revision": "587b4babffcdf0b50ebc12e41470483d",
+    "url": "/abz-agency/static/media/banner-photo.587b4bab.webp"
+  },
+  {
+    "revision": "a118cbb97cd43cb41b1696ce34f02006",
+    "url": "/abz-agency/static/media/banner-photo.a118cbb9.jpg"
+  },
+  {
+    "revision": "71c64e9eee5e4d8e5a2dea98e17c1920",
+    "url": "/abz-agency/static/media/banner-photo_mobile.71c64e9e.jpg"
+  },
+  {
+    "revision": "a7db3f9fe069dce6a1df76b1a4a7861f",
+    "url": "/abz-agency/static/media/banner-photo_mobile.a7db3f9f.webp"
+  },
+  {
+    "revision": "011e714868f1bb381a7c7b0ed8500466",
+    "url": "/abz-agency/static/media/banner-photo_tablet.011e7148.jpg"
+  },
+  {
+    "revision": "8d62c6b8b87037242d6d8d5b06403068",
+    "url": "/abz-agency/static/media/banner-photo_tablet.8d62c6b8.webp"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/abz-agency/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3a41f082d05e2a2756c67f67f21b2324",
+    "url": "/abz-agency/static/media/photo-cover.3a41f082.svg"
+  }
+]);
