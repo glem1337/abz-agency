0b3a9cab47cd78ee0dfc95491bdbaf98f590c764
Evhenii Grigoriev <grigorieffevgeniy@gmail.com>
2020-05-13 21:23:37 +0300

    Updates

--- precache-manifest.36d0d203a077213d3c8d2427763f0a4d.js
+++ precache-manifest.36d0d203a077213d3c8d2427763f0a4d.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "66473f75950834b3f3e69a6a5039a32d",
+    "revision": "be14f07df9b62a2e91f2341a240c7583",
     "url": "/abz-agency/index.html"
   },
   {
-    "revision": "babac02ea7e23e36b106",
+    "revision": "51357e8f4040b1bbeadc",
     "url": "/abz-agency/static/css/2.451bf2e4.chunk.css"
   },
   {
-    "revision": "6e99ba7384a6379a83e2",
-    "url": "/abz-agency/static/css/main.18cc987b.chunk.css"
+    "revision": "6b0de4780560b95d897d",
+    "url": "/abz-agency/static/css/main.3ba316c2.chunk.css"
   },
   {
-    "revision": "babac02ea7e23e36b106",
-    "url": "/abz-agency/static/js/2.fc7c313a.chunk.js"
+    "revision": "51357e8f4040b1bbeadc",
+    "url": "/abz-agency/static/js/2.9fe80eb1.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/abz-agency/static/js/2.fc7c313a.chunk.js.LICENSE.txt"
+    "url": "/abz-agency/static/js/2.9fe80eb1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6e99ba7384a6379a83e2",
-    "url": "/abz-agency/static/js/main.c729c2e3.chunk.js"
+    "revision": "6b0de4780560b95d897d",
+    "url": "/abz-agency/static/js/main.311e92a9.chunk.js"
   },
   {
     "revision": "97ac0c5e8c7376e5271b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/abz-agency/precache-manifest.6329a33d9507645327b2619ef3b017d7.js"
+  "/abz-agency/precache-manifest.36d0d203a077213d3c8d2427763f0a4d.js"
 );
 
 self.addEventListener('message', (event) => {
